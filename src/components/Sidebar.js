f9f16231b88491fe0050024d961b1584c7e87f6e
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-18 13:12:42 +0200

    added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 // import links
 import { Link } from 'react-router-dom';
 // import icons
-import { GrClose } from 'react-icons/gr';
+import { IoMdClose } from 'react-icons/io';
 // import aside context
 import { SidebarContext } from '../contexts/SidebarContext';
 // import cart context
@@ -25,10 +25,10 @@ const Sidebar = () => {
       } bg-white shadow-2xl fixed top-0 w-[420px] h-full transition-all duration-300 z-20`}
     >
       {/* header */}
-      <header className='bg-gray-100 py-8 px-10 flex justify-between'>
+      <header className='bg-black text-white py-6 px-10 flex justify-between'>
         <div className='uppercase font-medium'>Shopping Bag({itemAmount})</div>
-        <div onClick={handleClose} className='cursor-pointer '>
-          <GrClose className='text-[18px]' />
+        <div onClick={handleClose} className='cursor-pointer'>
+          <IoMdClose className='text-xl' />
         </div>
       </header>
       <div className='flex flex-col gap-y-2 px-10 py-[30px] max-h-[800px] overflow-y-auto'>
@@ -36,7 +36,10 @@ const Sidebar = () => {
         {cart.map((item) => {
           const { id, title, image, amount } = item;
           return (
-            <div className='flex justify-between gap-x-5 py-5' key={id}>
+            <div
+              className='flex justify-between gap-x-5 py-5 border-b border-gray-300'
+              key={id}
+            >
               <div>
                 <img className='max-w-[80px]' src={image} alt='' />
               </div>
@@ -44,31 +47,36 @@ const Sidebar = () => {
                 {/* title */}
                 <div className='mb-2 font-medium'>{title}</div>
                 <div className='flex justify-between items-center'>
-                  <div className='flex items-center w-[56px] justify-between text-base text-gray-500 font-medium'>
-                    <div
-                      className='text-xl cursor-pointer'
-                      onClick={() => decreaseAmount(id)}
-                    >
-                      -
+                  <div className='bg-pink-200 flex items-center gap-x-4'>
+                    <div className='flex items-center w-[56px] justify-between text-sm text-gray-500'>
+                      <div
+                        className='text-xl cursor-pointer'
+                        onClick={() => decreaseAmount(id)}
+                      >
+                        -
+                      </div>
+                      <div>{amount}</div>
+                      <div
+                        className='text-xl cursor-pointer'
+                        onClick={() => increaseAmount(id)}
+                      >
+                        +
+                      </div>
                     </div>
-                    <div>{amount}</div>
-                    <div
-                      className='text-xl cursor-pointer'
-                      onClick={() => increaseAmount(id)}
-                    >
-                      +
-                    </div>
+                    <div className='text-sm text-gray-500'>${item.price}</div>
                   </div>
-                  {/* price */}
-                  <div className='text-base font-medium'>${item.price}</div>
+                  <div className='text-base font-medium'>
+                    {/* round number at 2 decimal places */}$
+                    {parseFloat(item.price * amount).toFixed(2)}
+                  </div>
                 </div>
               </div>
               <div
-                className='cursor-pointer text-2xl'
+                className='cursor-pointer text-xl'
                 onClick={() => removeFromCart(id)}
               >
                 <div>
-                  <GrClose className='text-sm text-red-400' />
+                  <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
                 </div>
               </div>
             </div>
