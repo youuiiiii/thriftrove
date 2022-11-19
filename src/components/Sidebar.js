d771da35aa471c8112c1ffa12ec16b1ef88480c4
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-19 10:47:03 +0200

    added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,14 +2,11 @@ import React, { useContext } from 'react';
 // import links
 import { Link } from 'react-router-dom';
 // import icons
-import {
-  IoMdClose,
-  IoMdAdd,
-  IoMdRemove,
-  IoMdArrowForward,
-} from 'react-icons/io';
+import { IoMdArrowForward } from 'react-icons/io';
 import { FiTrash2 } from 'react-icons/fi';
-// import aside context
+// import components
+import Product from './Product';
+// import sidebar context
 import { SidebarContext } from '../contexts/SidebarContext';
 // import cart context
 import { CartContext } from '../contexts/CartContext';
@@ -28,7 +25,7 @@ const Sidebar = () => {
     <div
       className={`${
         isOpen ? 'right-0' : '-right-full'
-      } bg-white shadow-2xl fixed top-0 w-[480px] h-full transition-all duration-300 z-20 px-[35px]`}
+      } bg-white shadow-2xl fixed top-0 w-[90vw] lg:w-[30vw] h-full transition-all duration-300 z-20 px-[35px]`}
     >
       <div className='bg-white border-b py-6 flex justify-between items-center'>
         <div className='uppercase text-sm font-semibold'>
@@ -38,73 +35,9 @@ const Sidebar = () => {
           <IoMdArrowForward className='text-2xl' />
         </div>
       </div>
-      <div className='flex flex-col gap-y-2 h-[640px] overflow-y-auto overflow-x-hidden border-b'>
+      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
         {cart.map((item) => {
-          const { id, title, image, price, amount } = item;
-          return (
-            <div
-              className='flex gap-x-4 py-2 px-6 border-b border-gray-200 w-full font-light text-gray-500'
-              key={id}
-            >
-              <div className='w-full min-h-[150px] flex items-center  gap-x-4'>
-                {/* image */}
-                <div>
-                  <img className='max-w-[80px]' src={image} alt='' />
-                </div>
-
-                <div className='w-full flex flex-col'>
-                  {/* title & remove item icon */}
-                  <div className='flex justify-between mb-2'>
-                    {/* title */}
-                    <div className='mb-4 text-sm uppercase font-medium max-w-[240px] text-primary'>
-                      {title}
-                    </div>
-                    {/* remove item icon */}
-                    <div
-                      className='cursor-pointer text-xl'
-                      onClick={() => removeFromCart(id)}
-                    >
-                      <div>
-                        <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
-                      </div>
-                    </div>
-                  </div>
-                  <div className='flex gap-x-2 h-[36px] text-sm'>
-                    {/* quantity */}
-                    <div className='flex flex-1 items-center h-full border text-primary font-medium'>
-                      {/* icon remove */}
-                      <div
-                        onClick={() => decreaseAmount(id)}
-                        className='flex-1 h-full flex justify-center items-center cursor-pointer'
-                      >
-                        <IoMdRemove />
-                      </div>
-                      {/* amount */}
-                      <div className='h-full flex justify-center items-center px-2'>
-                        {amount}
-                      </div>
-                      {/* icon add */}
-                      <div
-                        onClick={() => increaseAmount(id)}
-                        className='flex-1 h-full flex justify-center items-center cursor-pointer'
-                      >
-                        <IoMdAdd />
-                      </div>
-                    </div>
-                    {/* item price */}
-                    <div className='flex-1 flex items-center justify-around'>
-                      $ {price}
-                    </div>
-                    {/* final price */}
-                    <div className='flex-1 flex justify-end items-center text-primary font-medium'>
-                      {/* make the price at 2 decimals */}
-                      {`$ ${parseFloat(item.price * amount).toFixed(2)}`}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
+          return <Product item={item} key={item.id} />;
         })}
       </div>
       <div className='flex flex-col gap-y-3 py-4 mt-4'>
