9b74eb6f33b3b8f9efab3421e4505c037492bf54
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-19 22:07:45 +0200

    added

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -26,20 +26,20 @@ const ProductDetails = () => {
   // destructure product
   const { title, price, description, image, category } = product;
   return (
-    <section className='py-32 h-screen flex items-center'>
+    <section className='pt-32 pb-12 lg:py-32 h-screen flex items-center'>
       <div className='container mx-auto'>
-        <div className='flex items-center'>
-          <div className='flex-1 flex justify-center items-center'>
-            <img className='max-w-md' src={image} alt='' />
+        <div className='flex flex-col lg:flex-row items-center'>
+          <div className='flex-1 flex justify-center items-center mb-8 lg:mb-0'>
+            <img className='max-w-[200px] lg:max-w-sm' src={image} alt='' />
           </div>
-          <div className='flex-1'>
-            <h1 className='text-[26px] font-medium mb-2 max-w-[450px]'>
+          <div className='flex-1 text-center lg:text-left'>
+            <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>
               {title}
             </h1>
-            <div className='text-xl text-red-500 font-medium mb-8'>
+            <div className='text-xl text-red-500 font-medium mb-6'>
               $ {price}
             </div>
-            <p className='mb-12'>{description}</p>
+            <p className='mb-8'>{description}</p>
             <button
               onClick={() => addToCart(product, product.id)}
               className='bg-primary py-4 px-8 text-white'
