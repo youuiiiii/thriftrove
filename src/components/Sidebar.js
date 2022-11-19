f3d43c730725e4326f16fa7ec7d3717a8d1f7bea
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-19 10:00:25 +0200

    added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -37,25 +37,25 @@ const Sidebar = () => {
           <IoMdArrowForward className='text-2xl' />
         </div>
       </div>
-      <div className='flex flex-col gap-y-2 py-[30px] h-[600px] overflow-y-auto overflow-x-hidden border-b'>
+      <div className='flex flex-col gap-y-2 h-[600px] overflow-y-auto overflow-x-hidden border-b'>
         {cart.map((item) => {
           const { id, title, image, price, amount } = item;
           return (
             <div
-              className='flex gap-x-4 py-4 px-2 border-b border-gray-200 bg-purple-100 w-full'
+              className='flex gap-x-4 py-2 px-6 border-b border-gray-200 w-full font-light text-gray-500'
               key={id}
             >
-              <div className='w-full min-h-[160px] flex gap-x-3'>
+              <div className='w-full min-h-[150px] flex items-center  gap-x-4'>
                 {/* image */}
                 <div>
                   <img className='max-w-[80px]' src={image} alt='' />
                 </div>
 
-                <div className='bg-red-300 w-full flex flex-col'>
+                <div className='w-full flex flex-col'>
                   {/* title & remove item icon */}
-                  <div className='flex justify-between'>
+                  <div className='flex justify-between mb-2'>
                     {/* title */}
-                    <div className='mb-4 text-sm uppercase font-medium max-w-[240px]'>
+                    <div className='mb-4 text-sm uppercase font-medium max-w-[240px] text-primary'>
                       {title}
                     </div>
                     {/* remove item icon */}
@@ -68,28 +68,36 @@ const Sidebar = () => {
                       </div>
                     </div>
                   </div>
-                  <div className='flex gap-x-2 h-[40px]'>
+                  <div className='flex gap-x-2 h-[36px] text-sm'>
                     {/* quantity */}
-                    <div className='flex bg-red-500 flex-1 items-center h-full'>
+                    <div className='flex flex-1 items-center h-full border text-primary font-medium'>
                       {/* icon remove */}
-                      <div className='flex-1 bg-yellow-300 h-full flex justify-center items-center'>
+                      <div
+                        onClick={() => decreaseAmount(id)}
+                        className='flex-1 h-full flex justify-center items-center cursor-pointer'
+                      >
                         <IoMdRemove />
                       </div>
                       {/* amount */}
-                      <div className='flex-1 bg-yellow-300 h-full flex justify-center items-center'>
+                      <div className='h-full flex justify-center items-center px-2'>
                         {amount}
                       </div>
                       {/* icon add */}
-                      <div className='flex-1 bg-yellow-300 h-full flex justify-center items-center'>
+                      <div
+                        onClick={() => increaseAmount(id)}
+                        className='flex-1 h-full flex justify-center items-center cursor-pointer'
+                      >
                         <IoMdAdd />
                       </div>
                     </div>
                     {/* item price */}
-                    <div className='bg-yellow-300 flex-1'>{price}</div>
+                    <div className='flex-1 flex items-center justify-around'>
+                      $ {price}
+                    </div>
                     {/* final price */}
-                    <div className='bg-green-300 flex-1'>
+                    <div className='flex-1 flex justify-end items-center text-primary font-medium'>
                       {/* make the price at 2 decimals */}
-                      {parseFloat(item.price * amount).toFixed(2)}
+                      {`$ ${parseFloat(item.price * amount).toFixed(2)}`}
                     </div>
                   </div>
                 </div>
